14445752944e97856f249330dccf5fd245497cc0
nm <69852236+nilavam@users.noreply.github.com>
2022-06-17 18:07:51 +0200

    Change

--- trial 6.25.20 PM/action.php
+++
@@ -1,6 +0,0 @@
-<?php
-ini_set('display_errors', true);          
-$data=$_POST['textdata'];
-$echo  $data, ' ', $to;
-#mail('nilavametya.huntik@gmail.com','subject',$data,"From: nilavam@cmi.ac.in");
-?>
