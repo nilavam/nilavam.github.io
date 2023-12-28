72141a990c78f0194d0dca1346adafcf23e31b75
nilavam <nilava30@gmail.com>
2023-12-28 17:13:24 +0530

    h

--- testing/scripts.js
+++ testing/scripts.js
@@ -1,3 +1,17 @@
-function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-}
+document.addEventListener("DOMContentLoaded", function () {
+    // Dark mode toggle functionality
+    const darkModeToggle = document.getElementById("darkModeToggle");
+    const body = document.body;
+
+    darkModeToggle.addEventListener("change", function () {
+        body.classList.toggle("dark-mode");
+    });
+
+    // Mobile navigation toggle functionality
+    const mobileNavToggle = document.getElementById("mobileNavToggle");
+    const nav = document.querySelector("nav");
+
+    mobileNavToggle.addEventListener("click", function () {
+        nav.classList.toggle("show-mobile-nav");
+    });
+});
